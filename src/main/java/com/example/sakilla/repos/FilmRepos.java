263ee08506a9d998ead152daf394a6ae08eee39c
Vinay Sharma <vs00380@surrey.ac.uk>
2025-03-17 16:26:04 +0000

    Filter API now accepted length ranges, less than and greater than

--- src/main/java/com/example/sakilla/repos/FilmRepos.java
+++ src/main/java/com/example/sakilla/repos/FilmRepos.java
@@ -15,7 +15,7 @@ public interface FilmRepos extends JpaRepository<Film, Short> {
         @Query("SELECT f FROM Film f LEFT JOIN FETCH f.categories WHERE f.id = :id")
         Optional<Film> findByIdWithCategories(@Param("id") Short id);
 
-        @Query("SELECT f FROM Film f " +
+       /* @Query("SELECT f FROM Film f " +
                 "LEFT JOIN FETCH f.categories c " + // Fetch categories associated with the film
                 "WHERE (:categoryId IS NULL OR c.id = :categoryId) " + // Filter by categoryId
                 "AND (:duration IS NULL OR f.length = :duration) " +
@@ -26,7 +26,25 @@ public interface FilmRepos extends JpaRepository<Film, Short> {
                                       @Param("duration") Integer duration,
                                       @Param("rating") String rating,
                                       @Param("releaseYear") Integer releaseYear,
+                                      @Param("languageId") Integer languageId);*/
+
+        @Query("SELECT f FROM Film f " +
+                "LEFT JOIN FETCH f.categories c " + // Fetch categories associated with the film
+                "WHERE (:categoryId IS NULL OR c.id = :categoryId) " + // Filter by categoryId
+                "AND (:duration IS NULL OR " +
+                "( (:duration = 0 AND f.length < 60) " + // Filter for duration < 60
+                "OR (:duration = 1 AND f.length BETWEEN 60 AND 120) " + // Filter for 60 <= duration <= 120
+                "OR (:duration = 2 AND f.length > 120) )) " + // Filter for duration > 120
+                "AND (:rating IS NULL OR f.rating = :rating) " +
+                "AND (:releaseYear IS NULL OR f.releaseYear = :releaseYear) " +
+                "AND (:languageId IS NULL OR f.languageId = :languageId)")
+        List<Film> findFilmsByFilters(@Param("categoryId") Short categoryId,
+                                      @Param("duration") Integer duration, // Kept as Integer
+                                      @Param("rating") String rating,
+                                      @Param("releaseYear") Integer releaseYear,
                                       @Param("languageId") Integer languageId);
+
+
 
 }
 
